c216bdd764bb7d9eec16ba3db5524aff4288702f
Anthony Paz <douglas.paz@unah.hn>
2025-06-30 22:47:14 -0600

    Renombrar Form1 a MainForm y actualizar referencias
    Se ha cambiado el nombre de la clase principal de la aplicación de `Form1` a `MainForm`, creando nuevos archivos `MainForm.cs` y `MainForm.Designer.cs`. Los archivos `Form1.cs` y `Form1.Designer.cs` han sido eliminados.
    
    Además, se ha actualizado `Program.cs` para iniciar la aplicación con `MainForm` y se han realizado modificaciones en `MainForm.resx` para reflejar la nueva clase. Se añadieron las directivas `using` necesarias en `MainForm.cs` para asegurar que todas las dependencias estén correctamente referenciadas.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using ProyectoBD2.UI.Forms;
+
 namespace ProyectoBD2
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace ProyectoBD2
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
