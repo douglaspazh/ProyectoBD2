f8aa6d395d2f1e46e34f0cde9a2d9855f58c4ede
Anthony Paz <douglas.paz@unah.hn>
2025-07-29 02:57:42 -0600

    Mejorar gestión de fincas en la aplicación
    Se han añadido métodos en `FincaPresenter.cs` para manejar eventos de búsqueda, adición, edición, eliminación y guardado de fincas, mejorando la lógica de la interfaz de usuario. En `FincasRepository.cs`, se han implementado métodos para interactuar con la base de datos, incluyendo operaciones de obtención, adición, eliminación y actualización de fincas. Además, se ha definido la interfaz en `IFincaRepository.cs` para estandarizar las operaciones relacionadas con las fincas. Estos cambios optimizan la funcionalidad y la estructura del código.

---
+++ Repositories/Interfaces/IFincaRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoBD2.Models;
+
+namespace ProyectoBD2.Repositories.Interfaces
+{
+    public interface IFincaRepository
+    {
+        void AddFinca(Finca finca);
+        void UpdateFinca(Finca fincaId);
+        void DeleteFinca(int fincaId);
+        IEnumerable<Finca> GetAllFincas();
+        IEnumerable<Finca> GetByValue(string value);
+        IEnumerable<Finca> GetByProductorId(int productorId);
+    }
+}
