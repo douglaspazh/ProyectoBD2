9e11f4955855cf2e45d0817baecfd2c2fafcd50c
Alejandro <achavezi@unah.hn>
2025-07-31 00:55:23 -0600

    Agregar lotes

--- Data/SQL/ProcedimientosNuevos.sql
+++ Data/SQL/ProcedimientosNuevos.sql
@@ -5,6 +5,7 @@ use GrupoNo1
 --50003 correo invalido
 --50050 registro duplicado
 --50051 registro inexistente
+--50052 problemas logisticos
 create or alter procedure spValidarCorreo
 @correo varchar(50)
 as
@@ -94,6 +95,8 @@ create or alter procedure spCrearFinca
 @ExtencionTotal varchar(20)
 as
 	begin try
+		if (select COUNT(ProductorID) from productor where ProductorID = @PId) != 1
+				THROW 50051, 'No existe este productor', 1;
 		exec spValidarCampoVarchar 'Nombre', @Nombre, 0, 50;
 		exec spValidarDecimal 'Extension Total',@ExtencionTotal;
 		declare @ext DECIMAL(10,2)
@@ -101,8 +104,7 @@ as
 		begin transaction
 			declare @ID INT;
 			select @ID = ISNULL(MAX(FincaID), 0) + 1 from Finca
-			if (select COUNT(ProductorID) from productor where ProductorID = @PId) != 1
-				THROW 50051, 'No existe este registro.', 1;
+			
 			insert into Finca (FincaID, ProductorID,Nombre,ExtencionTotal) values (@ID, @PId,@Nombre,@ext)
 		commit transaction 
 	end try
@@ -112,6 +114,72 @@ as
 		SELECT ERROR_NUMBER() as Estado,ERROR_MESSAGE() AS Mensaje;
 	end catch
 go
+--Crear lote
+create or alter procedure spCrearLote
+@FincaID int,
+@Extencion varchar(20),
+@MaximoCosechas int,
+@TipoSuelo varchar(51) = null,
+@TipoDeRiego varchar(51) = null
+as
+	begin try
+		--Validaciones
+		if (select COUNT(FincaID) from finca where FincaID = @FincaID) != 1
+				THROW 50051, 'No existe esta finca', 1;
+		exec spValidarCampoVarchar 'Tipo de suelo', @TipoSuelo, 0, 50;
+		exec spValidarCampoVarchar 'Tipo de riego', @TipoDeRiego, 0, 50;
+		exec spValidarDecimal 'Extencion',@Extencion;
+
+		--Evaluacion del terreno disponible
+		declare @ext DECIMAL(10,2),@extotal DECIMAL(10,2),@sumext DECIMAL(10,2)
+
+		set @ext = CONVERT(DECIMAL(10,2), @Extencion);
+
+		select @extotal = ExtencionTotal from Finca where FincaID = @FincaID
+		
+		select @sumext = sum(Extencion) from finca as f
+		inner join lote as l on l.FincaID=f.FincaID
+		group by l.FincaID
+
+		declare @msg varchar(200)
+		
+		if isnull(@sumext,0.0)=@extotal
+		begin
+			set @msg = 'No hay terreno disponible';
+			THROW 50052, @msg, 1
+		end
+		if isnull(@sumext,0.0)+@ext>@extotal
+		begin
+			set @msg = concat('La extencion del terreno disponible es de ',@extotal-isnull(@sumext,0.0));
+			THROW 50052, @msg, 1
+		end
+		begin transaction
+			declare @ID INT;
+			select @ID = ISNULL(MAX(LoteID), 0) + 1 from Lote
+			
+			insert into Lote (LoteID, FincaID,Extencion,MaximoCosechas,TipoSuelo,TipoDeRiego) values (@ID, @FincaID,@ext,@MaximoCosechas,@TipoSuelo,@TipoDeRiego)
+		commit transaction 
+	end try
+	begin catch	
+		IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION; 
+		SELECT ERROR_NUMBER() as Estado,ERROR_MESSAGE() AS Mensaje;
+	end catch
+go
+
+--Ejemplo:
+--exec spCrearLote
+--@fincaid =4,
+--@extencion='0.60',
+--@maximocosechas=5,
+--@tiposuelo='humedo',
+--@tipoderiego='asdasd'
+
+--exec spCrearFinca
+--@PId = 1,
+--@Nombre = 'Dato de preuba para lote', 
+--@ExtencionTotal ='125.20'
+
 --Prueba del procedimento almacenado
 ---spCrearProductor 'Alejandro Josue', 'Chavez Irias', 'a', '97589431','alejnadro@unah.hn','0501200311847'
 
