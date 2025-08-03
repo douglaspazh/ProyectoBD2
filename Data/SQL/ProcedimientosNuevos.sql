b9ed7d2b6fa4d2249c5e468ecdd6908f37614b9e
carloshernandez02 <cahernandezn@unah.hn>
2025-08-03 00:29:36 -0600

    Actualizar spCrearCosecha para usar spValidarFecha

--- Data/SQL/ProcedimientosNuevos.sql
+++ Data/SQL/ProcedimientosNuevos.sql
@@ -273,60 +273,62 @@ as
 	end catch
 go
 --Procedimiento para crear una cosecha
-create or alter procedure spCrearCosecha 
-@LoteID	int,
-@CultivoID int,
-@FechaInicio date = null,
-@FechaFinal date = null,
-@CantidadCosechas int,	--Voy a tratar CantidadCosechas como ciclos de cosecha esperados de una cosecha :P
-@Precio	varchar(20)
-as
-	begin try
-		if not exists(select LoteID from Lote where LoteID = @LoteID)
-			THROW 50051, 'No existe el lote solicitado', 1;
-
-		if not exists(select CultivoID from Cultivo where CultivoID = @CultivoID)
-			THROW 50051, 'No existe el cultivo solicitado', 1;
+create or alter procedure spCrearCosecha   
+@LoteID int,  
+@CultivoID int,  
+@FechaInicio date = null, 
+@FechaFinal date = null,  
+@CantidadCosechas int, --Voy a tratar CantidadCosechas como ciclos de cosecha esperados de una cosecha :P  
+@Precio varchar(20)  
+as   
+	begin try    
+		if not exists(select LoteID from Lote where LoteID = @LoteID)     
+			THROW 50051, 'No existe el lote solicitado', 1;      
+			
+		if not exists(select CultivoID from Cultivo where CultivoID = @CultivoID)     
+		THROW 50051, 'No existe el cultivo solicitado', 1;  
 		
-		if @FechaInicio is null and @FechaFinal is not null
-			THROW 50004, 'No puede existir fecha final sin una fecha inicial', 1;
+		if @FechaInicio is not null
+			EXEC spValidarFecha @FechaInicio
 		
-		if @FechaInicio is not null and @FechaFinal is not null
-		begin
-			if @FechaInicio >= @FechaFinal
-			THROW 50004, 'La fecha final de la cosecha es igual o mas vieja que la fecha de inicio', 1;
-		end
-
-		if @CantidadCosechas = 0
-			THROW 50001, 'La cantidad esperada de cosechas debe ser mayor a 0', 1;
-	
-		if (select EstadoID from Cosecha where LoteID = @LoteID) = 20001
-			THROW 50052, 'El lote seleccionado esta ocupado por un cultivo en crecimiento', 1;
+		if @FechaFinal is not null
+			EXEC spValidarFecha @FechaInicio
 		
-		if (select EstadoID from Cosecha where LoteID = @LoteID) = 20002
-			THROW 50052, 'El lote seleccionado esta ocupado por un cultivo aun no cosechado y entregado', 1
+		if @FechaInicio is null and @FechaFinal is not null     
+			THROW 50004, 'No puede existir fecha final sin una fecha inicial', 1;        
 		
-		exec spValidarDecimal 'Precio', @Precio;
-		declare @p decimal(10,2);
-		set @p = convert(decimal(10,2), @Precio);
+		if @FechaInicio is not null and @FechaFinal is not null    
+			begin     
+				if @FechaInicio >= @FechaFinal     
+					THROW 50004, 'La fecha final de la cosecha es igual o mas vieja que la fecha de inicio', 1;    
+			end      
+			
+		if @CantidadCosechas = 0     
+			THROW 50001, 'La cantidad esperada de cosechas debe ser mayor a 0', 1;       
 		
-		BEGIN TRANSACTION
-		declare @ID int;
-		select @ID = ISNULL(MAX(CosechaID), 0)+1 from Cosecha
-		insert into Cosecha (CosechaID, LoteID, CultivoID, FechaInicio, FechaFinal, EstadoID, CantidadCosechas, Precio) 
-			values (@ID, @LoteID, @CultivoID, @FechaInicio, @FechaFinal, 20001, @CantidadCosechas, @p) 
-			SELECT '10000' as Estado, 'Se creo correctamente la cosecha' AS Mensaje;
-			--Se crea el registro asumiendo que el la cosecha esta recien plantada. Quizas mas adelante se puede manejar con un ComboBox en la pantalla pertinente 
-		COMMIT TRANSACTION
-	end try
-
-	begin catch
+		if (select EstadoID from Cosecha where LoteID = @LoteID) = 20001     
+			THROW 50052, 'El lote seleccionado esta ocupado por un cultivo en crecimiento', 1;        
 		
-		if @@TRANCOUNT > 0
-		ROLLBACK TRANSACTION
-		select ERROR_NUMBER() as Estado, ERROR_MESSAGE() as Mensaje;
-	end catch
-go
+		if (select EstadoID from Cosecha where LoteID = @LoteID) = 20002     
+			THROW 50052, 'El lote seleccionado esta ocupado por un cultivo aun no cosechado y entregado', 1        
+			
+		exec spValidarDecimal 'Precio', @Precio;    
+		declare @p decimal(10,2);    
+		set @p = convert(decimal(10,2), @Precio);        
+			BEGIN TRANSACTION    declare @ID int;    
+				select @ID = ISNULL(MAX(CosechaID), 0)+1 from Cosecha    
+				insert into Cosecha (CosechaID, LoteID, CultivoID, FechaInicio, FechaFinal, EstadoID, CantidadCosechas, Precio)      
+				values (@ID, @LoteID, @CultivoID, @FechaInicio, @FechaFinal, 20001, @CantidadCosechas, @p) --Se crea el registro asumiendo que el la cosecha esta recien plantada. Quizas mas adelante se puede manejar con un ComboBox en la pantalla pertinente
+				SELECT '10000' as Estado, 'Se creo correctamente la cosecha' AS Mensaje;         
+			COMMIT TRANSACTION   
+		end try     
+		begin catch        
+			if @@TRANCOUNT > 0    
+				ROLLBACK TRANSACTION    
+			select ERROR_NUMBER() as Estado, 
+		ERROR_MESSAGE() as Mensaje;   
+		end catch
+GO
 --Procedimiento para crear productos
 create or alter procedure spCrearProducto
 @ProductoID varchar(13),
