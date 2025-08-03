c7a721f86c8bbc84c652999c073a0bb231eb9592
Anthony Paz <douglas.paz@unah.hn>
2025-08-02 20:08:57 -0600

    Mejorar manejo de errores en procedimientos de Productor
    Se han añadido bloques `BEGIN TRY` y `BEGIN CATCH` en los procedimientos `spDeleteProductor`, `spInsertProductor` y `spUpdateProductor` para un manejo de errores más robusto. Se implementaron validaciones de campos de entrada utilizando `spValidarCampoVarchar` y se aseguraron transacciones para las operaciones de eliminación, inserción y actualización. Estos cambios garantizan que las operaciones sobre la tabla `Productor` se realicen de manera segura y controlada.

--- Data/SQL/Stored Procedures/spDeleteProductor.sql
+++ Data/SQL/Stored Procedures/spDeleteProductor.sql
@@ -3,6 +3,20 @@ GO
 CREATE OR ALTER PROCEDURE spDeleteProductor
     @ID   INT
 AS
-    DELETE FROM Productor
-    WHERE ProductorID = @ID;
+    BEGIN TRY
+        -- Validación de existencia del productor
+        IF NOT EXISTS (SELECT 1 FROM Productor WHERE ProductorID = @ID)
+            THROW 50051, 'El productor no existe.', 1;
+
+        -- Eliminación del productor
+        BEGIN TRANSACTION
+            DELETE FROM Productor WHERE ProductorID = @ID;
+            SELECT 10000 AS Estado, 'Productor eliminado correctamente' AS Mensaje;
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        SELECT ERROR_NUMBER() AS Estado, ERROR_MESSAGE() AS Mensaje;
+    END CATCH
 GO
