85e2f10a6e4f2177d5c5027e8f0ed744942d922f
carloshernandez02 <cahernandezn@unah.hn>
2025-08-03 00:39:35 -0600

    Separar spCrearCuentaEntidad en procedimientos distintos y hacer que spCrearCuentaBancaria tenga output

--- Data/SQL/Stored Procedures/spCrearCuentaBancaria.sql
+++ Data/SQL/Stored Procedures/spCrearCuentaBancaria.sql
@@ -1,26 +1,27 @@
-
 create or alter procedure spCrearCuentaBancaria
 @NumeroCuenta varchar(25),
-@BancoID int
+@BancoID int,
+@CuentaID int output
 as
 	BEGIN TRY
-		EXEC spValidarCampoInt @BancoID, 'BancoID'
-		EXEC spValidarCampoVarchar @NumeroCuenta, 'Numero de cuenta', 10, 14
+		EXEC spValidarCampoVarchar 'Numero de cuenta', @NumeroCuenta, 10, 14
 
 		if exists(select NumeroCuenta from CuentaBancaria where NumeroCuenta=@NumeroCuenta)
 			THROW 50050, 'Este registro ya existe', 1;
+		
 		if not exists(select BancoID from Banco where BancoID = @BancoID)
 			THROW 50051, 'El Banco solicitado no existe', 1;
+		
 		BEGIN TRANSACTION
-			declare @ID int
-			select @ID = ISNULL(MAX(CuentaID),0)+1 from CuentaBancaria
+			select @CuentaID = ISNULL(MAX(CuentaID),0)+1 from CuentaBancaria
 			insert into CuentaBancaria (CuentaID,NumeroCuenta,BancoID)
-			values (@ID, @NumeroCuenta, @BancoID)
+			values (@CuentaID, @NumeroCuenta, @BancoID)
 		COMMIT TRANSACTION
 		SELECT 10000 as Estado, 'Cuenta bancaria creada exitosamente' as Mensaje
 	END TRY
 	BEGIN CATCH
-	IF @@TRANCOUNT > 0
+		IF @@TRANCOUNT > 0
 			ROLLBACK TRANSACTION;
 		SELECT ERROR_NUMBER() as Estado, ERROR_MESSAGE() as Mensaje
 	END CATCH
+GO
--- Data/SQL/Stored Procedures/spCrearCuentaEntidad.sql
+++ Data/SQL/Stored Procedures/spCrearCuentaEntidad.sql
@@ -1,54 +1,54 @@
-CREATE OR ALTER PROCEDURE spCrearCuentaEntidad
-@CuentaID INT,
-@EntidadID INT,
-@TipoEntidad VARCHAR(20) -- 'Productor' or 'Proveedor'
-AS
-BEGIN TRY
-	EXEC spValidarCampoInt @CuentaID, 'CuentaID'
-	EXEC spValidarCampoInt @EntidadID, 'EntidadID'
+--Procedimiento crear cuenta para Productores
+create or alter procedure spCrearCuentaProductor
+@ProductorID int,
+@BancoID int,
+@NumeroCuenta varchar(25)
+as
+	BEGIN TRY
+		if not exists(select ProductorID from Productor where ProductorID=@ProductorID)
+			THROW 50051, 'El productor referenciado no existe', 1;
+		
+		declare @CuentaID int;
 
-	if not exists (SELECT 1 FROM CuentaBancaria WHERE CuentaID = @CuentaID)
-		THROW 50051, 'La cuenta bancaria no existe', 1;
-	
-	declare @ID INT;
-	if @TipoEntidad = 'Productor'
-		BEGIN
-			if not exists (SELECT 1 FROM Productor WHERE ProductorID = @EntidadID)
-				THROW 50051, 'El Productor referenciado no existe', 1;
-	
-			if exists (SELECT 1 FROM CuentaProductor WHERE CuentaID = @CuentaID AND ProductorID = @EntidadID)
-				THROW 50050, 'Ya existe esta relación', 1;
-	
-			BEGIN TRANSACTION
-				select @ID = ISNULL(MAX(CuentaProductorID),0)+1 FROM CuentaProductor;
-				insert into CuentaProductor (CuentaProductorID, CuentaID, ProductorID)
-				values (@ID, @CuentaID, @EntidadID);
-			COMMIT TRANSACTION
-	
-			SELECT 10000 as Estado, 'Cuenta asociada al productor exitosamente' as Mensaje;
-		END
-		ELSE IF @TipoEntidad = 'Proveedor'
-		BEGIN
-			if not exists (SELECT 1 FROM Proveedor WHERE ProveedorID = @EntidadID)
-				THROW 50051, 'El Proveedor referenciado no existe', 1;
+		EXEC spCrearCuentaBancaria @NumeroCuenta, @BancoID, @CuentaID output
 
-			if exists (SELECT 1 FROM CuentaProveedor WHERE CuentaID = @CuentaID AND ProveedorID = @EntidadID)
-				THROW 50050, 'Ya existe esta relación', 1;
-	
-			BEGIN TRANSACTION
-				select @ID = ISNULL(MAX(CuentaProveedorID),0)+1 FROM CuentaProveedor;
-				insert into CuentaProveedor (CuentaProveedorID, CuentaID, ProveedorID)
-				values (@ID, @CuentaID, @EntidadID);
-			COMMIT TRANSACTION
-	
-			SELECT 10000 as Estado, 'Cuenta asociada al proveedor exitosamente' as Mensaje;
-		END
-		ELSE
-			THROW 50005, 'Tipo de entidad no válido. Debe ser ''Productor'' o ''Proveedor''.', 1;
+		BEGIN TRANSACTION
+			insert into CuentaProductor (CuentaID,ProductorID)
+			values (@CuentaID, @ProductorID)
+		COMMIT TRANSACTION
+		SELECT 10000 as Estado, 'Cuenta de productor creada exitosamente' as Mensaje
 	END TRY
 	BEGIN CATCH
 		IF @@TRANCOUNT > 0
 			ROLLBACK TRANSACTION;
-		SELECT ERROR_NUMBER() AS Estado, ERROR_MESSAGE() AS Mensaje;
+		SELECT ERROR_NUMBER() as Estado, ERROR_MESSAGE() as Mensaje
+	END CATCH
+GO 
+
+--Procedimiento crear cuenta para Proveedores
+create or alter procedure spCrearCuentaProveedor
+@ProveedorID int,
+@BancoID int,
+@NumeroCuenta varchar(25)
+as
+	BEGIN TRY
+	
+		if not exists(select ProveedorID from Proveedor where ProveedorID=@ProveedorID)
+			THROW 50051, 'El Proveedor referenciado no existe', 1;
+
+		declare @CuentaID int;
+
+		EXEC spCrearCuentaBancaria @NumeroCuenta, @BancoID, @CuentaID output
+	
+		BEGIN TRANSACTION
+			insert into CuentaProveedor (CuentaID,ProveedorID)
+			values (@CuentaID, @ProveedorID)
+		COMMIT TRANSACTION
+		SELECT 10000 as Estado, 'Cuenta de Proveedor creada exitosamente' as Mensaje
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+			ROLLBACK TRANSACTION;
+		SELECT ERROR_NUMBER() as Estado, ERROR_MESSAGE() as Mensaje
 	END CATCH
 GO
