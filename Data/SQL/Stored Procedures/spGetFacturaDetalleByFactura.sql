4c74bd46c7b65767ea156275c248a71066820689
carloshernandez02 <cahernandezn@unah.hn>
2025-08-03 22:39:57 -0600

    Agregar procedimientos Facturas

--- Data/SQL/Stored Procedures/spGetFacturaDetalleByFactura.sql
+++ Data/SQL/Stored Procedures/spGetFacturaDetalleByFactura.sql
@@ -1,1 +1,12 @@
-﻿
+﻿create or alter procedure spGetFacturaDetalleByFactura
+@FacturaID int
+as
+	select * into #Factura from Factura where FacturaID = @FacturaID
+	select * into #FacturaDetalle from FacturaDetalle where FacturaDetalleID = @FacturaID
+	
+	select  p.Nombre, p.ProductoID, fd.Cantidad, p.UnidadMedida, c.Nombre as Categoria, fd.BodegaID as Bodega
+	from #Factura as f 
+	inner join #FacturaDetalle as fd on f.FacturaID = fd.FacturaID
+	inner join Producto as p on fd.ProductoID = p.ProductoID
+	inner join Categoria as c on p.CategoriaID = c.CategoriaID
+go
