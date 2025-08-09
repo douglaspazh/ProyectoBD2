e2cf7bbd5774db2d9dd99525dcd39e3525fe473a
Daniela-Nicolle-Ramos <dnramosv@unah.hn>
2025-08-08 22:57:53 -0600

    Form Factura

--- Views/Forms_Transacciones/FacturasView.cs
+++ Views/Forms_Transacciones/FacturasView.cs
@@ -16,5 +16,10 @@ namespace ProyectoBD2.Views.Forms_Transacciones
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
